91d528669726d41b0877d5caddd8a0c36bc0e903
GavrilS <gavr1lll@abv.bg>
2023-11-09 21:59:34 +0200

    Added CarOffer item to the items file

--- offer_crawler/crawler/crawler/items.py
+++ offer_crawler/crawler/crawler/items.py
@@ -10,3 +10,9 @@ class CrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class CarOffer(scrapy.Item):
+    title = scrapy.Field()
+    price = scrapy.Field()
+    details = scrapy.Field()
