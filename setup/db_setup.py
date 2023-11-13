5f08638ec52408b2240c53c9a41c0fbe4114e32a
GavrilS <gavr1lll@abv.bg>
2023-11-13 08:25:16 +0200

    Fixed db connection and added extra prints to verify what is happening

--- setup/db_setup.py
+++ setup/db_setup.py
@@ -5,14 +5,13 @@ import psycopg2
 
 def db_connect(db_name, db_user, db_pass):
     try:
-        conn = psycopg2.connect(f"dbname={db_name} user={db_user} password={db_pass}")
-        # conn = psycopg2.connect(
-        #     host="localhost",
-        #     database=db_name,
-        #     user=db_user,
-        #     password=db_pass,
-        #     port='5432'
-        # )
+        conn = psycopg2.connect(
+            host="localhost",
+            database=db_name,
+            user=db_user,
+            password=db_pass,
+            port='5432'
+        )
         print('Connection set up successfully!!!')
         return conn
     except Exception as e:
@@ -24,8 +23,9 @@ def execute_db_setup(conn):
     try:
         cur = conn.cursor()
         cur.execute(open("car_offer_db_schema.sql", "r").read())
-        curr.close()
-        # conn.commit()
+        cur.close()
+        conn.commit()
+        print('Operations executed and commited!')
     except Exception as e:
         print('There was an error trying to execute to set up .sql commands: ', str(e))
     finally:
@@ -34,6 +34,7 @@ def execute_db_setup(conn):
 
 def close_db_connection(conn):
     conn.close()
+    print('Connection closed!')
 
 
 if __name__=='__main__':
@@ -43,4 +44,9 @@ if __name__=='__main__':
     user = os.getenv('POSTGRES_USER', None)
     password = os.getenv('POSTGRES_PASSWORD', None)
 
+    # print('db: ', db)
+    # print('user: ', user)
+    # print('password: ', password)
+
     conn = db_connect(db, user, password)
+    execute_db_setup(conn)
