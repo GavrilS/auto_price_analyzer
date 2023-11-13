bf34b6c433a210b34a171ce3fe7fc723ff505589
GavrilS <gavr1lll@abv.bg>
2023-11-13 22:08:47 +0200

    Creating pipeline for handling scraped offers: WIP

--- offer_crawler/crawler/crawler/pipelines.py
+++ offer_crawler/crawler/crawler/pipelines.py
@@ -5,9 +5,41 @@
 
 
 # useful for handling different item types with a single interface
+import os
 from itemadapter import ItemAdapter
+import psycopg2
+
+DB_HOST = os.getenv('DB_HOST', 'localhost')
+DB_PORT = os.getenv('DB_PORT', '5432')
+DB_USER = os.getenv('DB_USER', None)
+DB_PASS = os.getenv('DB_PASS', None)
+DB_NAME = os.getenv('DB_NAME', None)
+
+# class CrawlerPipeline:
+#     def process_item(self, item, spider):
+#         return item
 
 
-class CrawlerPipeline:
+class OfferPipeline:
+    def open_spider(self, spider):
+        try:
+            self.conn = psycopg2.connect(
+                host=DB_HOST,
+                database=DB_NAME,
+                user=DB_USER,
+                password=DB_PASS,
+                port=DB_PORT
+            )
+            print('DB connection set up successfully!')
+        except Exception as e:
+            print('Could not set up connection to the DB: ', str(e))
+
+
+    def close_spider(self, spider):
+        self.conn.close()
+        print('The connection to the DB was closed successfully!')
+
+    
     def process_item(self, item, spider):
-        return item
+        
+        pass
