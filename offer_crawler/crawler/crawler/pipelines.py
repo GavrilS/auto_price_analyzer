2350bb585a044483258825dfeefaf359703145eb
GavrilS <gavr1lll@abv.bg>
2023-11-28 21:18:33 +0200

    Added the logic to update offer records with new data if the records are already present and move the old data to the history table for analyzing

--- offer_crawler/crawler/crawler/pipelines.py
+++ offer_crawler/crawler/crawler/pipelines.py
@@ -56,18 +56,39 @@ class OfferPipeline:
         price = item.get('price', DEFAULT_PRICE)
         details = item.get('details', DEFAULT_DETAILS)
         record_time = item.get('record_time', DEFAULT_RECORD_TIME)
-        get_offer_query = f"SELECT id FROM offer WHERE user_id = {user_id} and title = {title}"
+        get_offer_query = f"SELECT * FROM offer WHERE user_id = {user_id} and title = {title}"
         result = self.cur.execute(get_offer_query).fetchone()
+        print('result: ', result)
+        # No result means this offer is being saved for the first time for this user
+        # Insert it into offer table and create a corresponding record to the history table
         if not result:
             insert_offer_query = f"INSERT INTO offer (title, price, details, record_time, user_id) VALUES 
-            ({title}, {price}, {details}, {record_time}, {user_id} RETURNING id)"
+            ({title}, {price}, {details}, {record_time}, {user_id}) RETURNING id"
             self.cur.execute(insert_offer_query)
-            offer_id = self.cur.fetchone()[0]
+            offer_id = self.cur.fetchone()
+            print('offer_id: ', offer_id)
             prices = [price]
             record_times = [record_time]
             insert_history_query = f"INSERT INTO history (offer_id, prices, record_times) VALUES
             ({offer_id}, {prices}, {record_times})"
             self.cur.execute(insert_history_query)
             self.conn.commit()
+        # If there is a result this means we have a record of this offer already
+        # Update the old record with the new values and save the old values in the history table
         else:
-            pass
+            get_history_query = f"SELECT id, prices, record_times FROM history WHERE offer_id = {result[0]}"
+            self.cur.execute(get_history_query)
+
+            history_record = self.cur.fetchone()
+            print('history_record: ', history_record)
+            history_record[1].append(result[2])
+            history_record[2].append(result[4])
+
+            update_history_query = f"INSERT INTO history (prices, record_times) VALUES
+            ({history_record[1]}, {history_record[2]}) WHERE id = {history_record[0]})"
+            self.cur.execute(update_history_query)
+
+            update_offer_query = f"INSERT INTO offer (title, price, details, record_time, user_id) VALUES 
+            ({title}, {price}, {details}, {record_time}, {user_id}) WHERE id = {result[0]}"
+            self.cur.execute(update_offer_query)
+            self.con.commit()
